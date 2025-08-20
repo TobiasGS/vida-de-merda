46414d30138e489f6259e410442e3802924b0daa
acesso43491247 <tobias.gomes@goldencloud.tech>
2025-08-20 08:25:56 -0300

    add supbaseclient

---
+++ vida-de-merda/src/supabaseClient.js
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://sadehqajqbjudckhsusr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhZGVocWFqcWJqdWRja2hzdXNyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU2MDc0OTcsImV4cCI6MjA3MTE4MzQ5N30.etOHthFoKGOP354xT0U3GpjlbAc5MSTugR2O_vwuJ_E'
+export const supabase = createClient(supabaseUrl, supabaseKey)
